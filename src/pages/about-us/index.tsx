977bca03143d425af818fe84dc769572e8b5ebdb
Santiago Vanegas <santvanegas11@gmail.com>
2023-04-30 15:04:59 -0500

    add about page

---
+++ src/pages/about-us/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from "@/components/header/Header";
+import { Container } from "@nextui-org/react";
+
+export default function AboutUs() {
+    return (
+        <Container>
+            <Header />
+        </Container>
+    )
+}
