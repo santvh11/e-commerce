a0a1745f80e9a2735ce42fbd8eae589ea4e6b290
Santiago Vanegas <santvanegas11@gmail.com>
2023-05-01 16:01:04 -0500

    add item component for carousel

--- src/components/item/Item.tsx
+++ src/components/item/Item.tsx
@@ -1,6 +1,29 @@
+import { Product } from '@/utils/types'
+import { Button, Card, Row, Text, useTheme } from '@nextui-org/react'
+import Link from 'next/link'
 
-export const Item = () => {
+interface Props {
+    data: Product
+}
+
+export const Item = ({ data }: Props) => {
+  const { theme } = useTheme()
   return (
-    <div>Item</div>
+    <Card css={{ w: '300px' }}>
+      <Link href={'/product/' + data.id}>
+        <Card.Header>
+          <Text>{data.title}</Text>
+        </Card.Header>
+        <Card.Image css={{ h: '300px', bc: theme?.colors.gray300.value }} src={data.images.at(0) || ''} />
+      </Link>
+      <Card.Footer>
+        <Row justify='space-around' align='center'>
+          <Button>
+            Add to cart
+          </Button>
+          <Text h5>${data.price}</Text>
+        </Row>
+      </Card.Footer>
+    </Card>
   )
 }
