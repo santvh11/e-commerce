b4d841e2ea83d7802668904facab1f6d385d155f
Santiago Vanegas <santvanegas11@gmail.com>
2023-04-30 21:59:12 -0500

    add basic header construction

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,13 @@
+import { Container, Link } from "@nextui-org/react"
+import NextLink from "next/link"
+import { Logo } from "../svgs/isologo/Isologo"
+import { SearchBar } from "../search-bar/SearchBar"
+import { Navbar } from "../navbar/Navbar"
+
 export const Header = () => {
     return (
-        <div>Header</div>
+        <header>
+            <Navbar />
+        </header>
     )
 }
