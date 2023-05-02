eec6f4b0408e35485bdfb58322176f005408280d
Santiago Vanegas <santvanegas11@gmail.com>
2023-05-02 05:30:57 -0500

    add search bar functionality

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,11 @@
-import { FormElement, Input, useTheme } from '@nextui-org/react'
+import { Button, Dropdown, FormElement, Input, Row, Text, useTheme } from '@nextui-org/react'
 import { ChangeEvent, useState } from 'react'
 import { SearchIcon } from './svgs/SearchIcon'
+import { Product } from '@/utils/types'
+import Link from 'next/link'
 
 interface Props {
-    data: any[]
+    data: Product[]
 }
 
 export const SearchBar = ({ data }: Props) => {
@@ -15,22 +17,48 @@ export const SearchBar = ({ data }: Props) => {
     setQuery(e.target.value)
   }
 
+  const mathcingResults = data.filter(product => product.title.toLowerCase().includes(query.toLowerCase()))
+
   return (
-    <Input
-      clearable
-      contentLeft={<SearchIcon color={theme?.colors.accents6.value} size={16} />}
-      contentLeftStyling={false}
-      css={{
-        w: '100%',
-        '@xsMax': {
-          mw: '300px'
-        },
-        '& .nextui-input-content--left': {
-          ml: '$5',
-          dflex: 'center'
-        }
-      }}
-      placeholder='Search...'
-    />
+    <Row>
+      <Input
+        clearable
+        contentLeftStyling={false}
+        onChange={handleOnChange}
+        css={{
+          zIndex: 99,
+          w: '100%',
+          '@xsMax': {
+            mw: '300px'
+          },
+          '& .nextui-input-content--left': {
+            ml: '$5',
+            dflex: 'center'
+          }
+        }}
+        placeholder='Search...'
+      />
+      <Dropdown>
+        <Dropdown.Trigger>
+          <Button auto light>
+            <SearchIcon
+              color={theme?.colors.white.value}
+              size={25}
+            />
+          </Button>
+        </Dropdown.Trigger>
+        <Dropdown.Menu items={mathcingResults}>
+          {(item) => (
+            <Dropdown.Item key={item.id}>
+              <Link href={'/product/' + item.id}>
+                <Text>
+                  {item.title}
+                </Text>
+              </Link>
+            </Dropdown.Item>
+          )}
+        </Dropdown.Menu>
+      </Dropdown>
+    </Row>
   )
 }
