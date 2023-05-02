e73121f8f96baf11a52346a4f0cf91761ff8193a
Santiago Vanegas <santvanegas11@gmail.com>
2023-05-01 23:12:52 -0500

    Add cart icon

---
+++ src/components/svgs/cart-icon/CartIcon.tsx
@@ -0,0 +1,93 @@
+export const CartIcon = ({ color = '#fff', size = 30 }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width={size}
+    viewBox='0 0 682.667 682.667'
+    xmlSpace='preserve'
+  >
+    <defs>
+      <clipPath id='a' clipPathUnits='userSpaceOnUse'>
+        <path
+          d='M0 512h512V0H0Z'
+          data-original='#000000'
+        />
+      </clipPath>
+    </defs>
+    <g
+      clipPath='url(#a)'
+      transform='matrix(1.33333 0 0 -1.33333 0 682.667)'
+      fill='none'
+      stroke={color}
+      strokeWidth={30}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeMiterlimit={10}
+    >
+      <path
+        d='M0 0h391l-60-210H60'
+        style={{
+          strokeWidth: 30,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeMiterlimit: 10,
+          strokeDasharray: 'none',
+          strokeOpacity: 1
+        }}
+        transform='translate(106 406)'
+        data-original='#000000'
+      />
+      <path
+        d='M0 0c0-16.568-13.432-30-30-30-16.568 0-30 13.432-30 30 0 16.568 13.432 30 30 30C-13.432 30 0 16.568 0 0Z'
+        style={{
+          strokeWidth: 30,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeMiterlimit: 10,
+          strokeDasharray: 'none',
+          strokeOpacity: 1
+        }}
+        transform='translate(256 76)'
+        data-original='#000000'
+      />
+      <path
+        d='M0 0c0-16.568-13.432-30-30-30-16.568 0-30 13.432-30 30 0 16.568 13.432 30 30 30C-13.432 30 0 16.568 0 0Z'
+        style={{
+          strokeWidth: 30,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeMiterlimit: 10,
+          strokeDasharray: 'none',
+          strokeOpacity: 1
+        }}
+        transform='translate(407 76)'
+        data-original='#000000'
+      />
+      <path
+        d='M0 0h-252.459c-22.301 0-36.806 23.469-26.833 43.417L-271 60'
+        style={{
+          strokeWidth: 30,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeMiterlimit: 10,
+          strokeDasharray: 'none',
+          strokeOpacity: 1
+        }}
+        transform='translate(437 136)'
+        data-original='#000000'
+      />
+      <path
+        d='M0 0h73.861C99.402-89.409 151-270 151-270'
+        style={{
+          strokeWidth: 30,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeMiterlimit: 10,
+          strokeDasharray: 'none',
+          strokeOpacity: 1
+        }}
+        transform='translate(15 466)'
+        data-original='#000000'
+      />
+    </g>
+  </svg>
+)
