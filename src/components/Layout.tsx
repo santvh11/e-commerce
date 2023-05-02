3726a2594ada9a924b0cbb6a009ca173f4122846
Santiago Vanegas <santvanegas11@gmail.com>
2023-05-02 04:46:01 -0500

    add layout needed data

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,11 +2,17 @@ import { Container } from '@nextui-org/react'
 import React from 'react'
 import { Footer } from './Footer'
 import { Navbar } from './Navbar'
+import { Product } from '@/utils/types'
 
-export const Layout = ({ children }) => {
+interface Props {
+  children: React.ReactElement[]
+  data: Product[]
+}
+
+export const Layout = ({ children, data }: Props) => {
   return (
-    <Container fluid css={{ pl: 0, pr: 0 }}>
-      <Navbar />
+    <Container fluid>
+      <Navbar data={data} />
       {children}
       <Footer />
     </Container>
