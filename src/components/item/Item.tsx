039364d80034ae26c782a6e19430f18cdf7ffcee
Santiago Vanegas <santvanegas11@gmail.com>
2023-05-01 23:20:34 -0500

    Add cart functionality to items

--- src/components/item/Item.tsx
+++ src/components/item/Item.tsx
@@ -1,13 +1,15 @@
 import { Product } from '@/utils/types'
 import { Button, Card, Row, Text, useTheme } from '@nextui-org/react'
 import Link from 'next/link'
-
+import { QuantitySelector } from '../quantity-selector/QuantitySelector'
+import { useCart } from '@/hooks/useCart'
 interface Props {
     data: Product
 }
-
 export const Item = ({ data }: Props) => {
   const { theme } = useTheme()
+  const { addToCart, changeQuantity, quantity } = useCart(data)
+
   return (
     <Card css={{ w: '300px' }}>
       <Link href={'/product/' + data.id}>
@@ -16,11 +18,18 @@ export const Item = ({ data }: Props) => {
         </Card.Header>
         <Card.Image css={{ h: '300px', bc: theme?.colors.gray300.value }} src={data.images.at(0) || ''} />
       </Link>
+
       <Card.Footer>
         <Row justify='space-around' align='center'>
-          <Button>
-            Add to cart
-          </Button>
+          {quantity
+            ? (
+              <QuantitySelector handleOnChange={changeQuantity} quantity={quantity} />
+              )
+            : (
+              <Button onClick={addToCart}>
+                Add to cart
+              </Button>
+              )}
           <Text h5>${data.price}</Text>
         </Row>
       </Card.Footer>
