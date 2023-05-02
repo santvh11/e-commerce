96a7af8efb50bc0a4603277cc7892cddc1f4bd32
Santiago Vanegas <santvanegas11@gmail.com>
2023-05-02 04:47:14 -0500

    delete not used pages

--- src/pages/pay/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Layout } from '@/components/Layout'
-
-export default function Pay () {
-  return (
-    <Layout>
-      <p>Page</p>
-    </Layout>
-  )
-}
--- src/pages/summary/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Layout } from '@/components/Layout'
-
-export default function Summary () {
-  return (
-    <Layout>
-      <p>Page</p>
-    </Layout>
-  )
-}
